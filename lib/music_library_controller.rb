93519c8868a824f858bca59b36df559ca66c48d7
yaoyu bao <yaobao5354@gmail.com>
2019-09-18 03:24:37 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -17,10 +17,10 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    input = gets.strip
-    while input != "exit"
-      input = gets.strip
-    end
+    #input = gets.strip
+    #while input != "exit"
+    #  input = gets.strip
+    #end
   end
 
   def list_songs

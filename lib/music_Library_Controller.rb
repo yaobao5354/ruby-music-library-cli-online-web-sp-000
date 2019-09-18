310c89dfcf65a85175c3e7bc293c7cf08e0d5327
yaoyu bao <yaobao5354@gmail.com>
2019-09-18 03:04:42 +0000

    Automatically backed up by Learn

--- lib/music_Library_Controller.rb
+++ lib/music_Library_Controller.rb
@@ -73,10 +73,10 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     #self.list_songs
-    input = gets.strip.to_i
-
+    input = gets.strip
+    input = input.to_i
     #binding.pry
-    
+
     if input.is_a?(Integer) == true
       list = Song.all.sort{|a,b| a.name <=> b.name}
       list.each.with_index do |song, index|
